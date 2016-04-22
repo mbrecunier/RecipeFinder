1fb247e3a0534946471e19d0ba7b3da13cb50500
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-22 10:07:30 -0700

    test that RecipesActivity loads from MainActivity button click

--- app/src/test/java/com/example/guest/recipefinder/MainActivityTest.java
+++ app/src/test/java/com/example/guest/recipefinder/MainActivityTest.java
@@ -1,5 +1,6 @@
 package com.example.guest.recipefinder;
 
+import android.content.Intent;
 import android.os.Build;
 import android.util.Log;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
 
 import static junit.framework.Assert.assertTrue;
 
@@ -29,5 +31,14 @@ public class MainActivityTest {
     public void validateTextViewContent() {
         TextView introTextView = (TextView) activity.findViewById(R.id.introTextView);
         assertTrue(("Find recipes using what you have in your kitchen").equals(introTextView.getText().toString()));
+    }
+
+    @Test
+    public void secondActivityStarted() {
+        activity.findViewById(R.id.findRecipesButton).performClick();
+        Intent expectedIntent = new Intent(activity, RecipesActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
     }
 }
