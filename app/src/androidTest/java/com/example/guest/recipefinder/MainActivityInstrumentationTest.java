a088187e67b932cb3ec2899cefabd1effbcf5d17
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-22 10:32:19 -0700

    test UI to validate textedit on MainActivity

---
+++ app/src/androidTest/java/com/example/guest/recipefinder/MainActivityInstrumentationTest.java
@@ -0,0 +1,23 @@
+package com.example.guest.recipefinder;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class MainActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.ingredient1EditText)).perform(typeText("lettuce")).check(matches(withText("lettuce")));
+    }
+}
