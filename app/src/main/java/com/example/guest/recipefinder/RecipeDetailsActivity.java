98ef6e436b593a1e885224d7aabc93d46ad4a6e6
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-22 09:16:02 -0700

    load in sample recipe picture

---
+++ app/src/main/java/com/example/guest/recipefinder/RecipeDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.example.guest.recipefinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_details);
+    }
+}
