d77386dcbcc7ea38c0a4c867aa53d62fa5248575
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-21 17:16:33 -0700

    create intent to pass user input from MainActivity to RecipesActivity

--- app/src/main/java/com/example/guest/recipefinder/MainActivity.java
+++ app/src/main/java/com/example/guest/recipefinder/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.guest.recipefinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.findRecipesButton) Button mFindRecipesButton;
+    @Bind(R.id.ingredient1EditText) EditText mIngredient1EditText;
+    @Bind(R.id.ingredient2EditText) EditText mIngredient2EditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mFindRecipesButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.findRecipesButton:
+                String ingredient1 = mIngredient1EditText.getText().toString();
+                String ingredient2 = mIngredient2EditText.getText().toString();
+                Intent recipesIntent = new Intent(MainActivity.this, RecipesActivity.class);
+                recipesIntent.putExtra("ingredient1", ingredient1);
+                recipesIntent.putExtra("ingredient2", ingredient2);
+                startActivity(recipesIntent);
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/guest/recipefinder/RecipesActivity.java
@@ -0,0 +1,13 @@
+package com.example.guest.recipefinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+    }
+}
