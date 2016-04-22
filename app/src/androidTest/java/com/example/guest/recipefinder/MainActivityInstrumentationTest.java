afb6ae1341c82463e8e026a672083ead9c944455
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-22 10:42:25 -0700

    test that ingredients are sent to RecipesActicity

--- app/src/androidTest/java/com/example/guest/recipefinder/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/example/guest/recipefinder/MainActivityInstrumentationTest.java
@@ -6,6 +6,8 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -19,5 +21,25 @@ public class MainActivityInstrumentationTest {
     @Test
     public void validateEditText() {
         onView(withId(R.id.ingredient1EditText)).perform(typeText("lettuce")).check(matches(withText("lettuce")));
+    }
+
+    @Test
+    public void ingredientsAreSentToRecipesActivity() {
+        String ingredient1 = "lettuce";
+        String ingredient2 = "broccoli";
+        onView(withId(R.id.ingredient1EditText)).perform(typeText(ingredient1), closeSoftKeyboard());
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.ingredient2EditText)).perform(typeText(ingredient2), closeSoftKeyboard());
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.findRecipesButton)).perform(click());
+        onView(withId(R.id.searchTextView)).check(matches(withText("With " + ingredient1 + " and " + ingredient2 + " you can make:")));
     }
 }
