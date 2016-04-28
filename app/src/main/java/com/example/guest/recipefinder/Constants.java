ee06b998299d9b7e66ca56350d5f652ab1bca728
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-28 16:52:39 -0700

    add constant for adding second ingredient

---
+++ app/src/main/java/com/example/guest/recipefinder/Constants.java
@@ -0,0 +1,13 @@
+package com.example.guest.recipefinder;
+
+/**
+ * Created by Guest on 4/28/16.
+ */
+public class Constants {
+    public static final String API_KEY = BuildConfig.API_KEY;
+
+    public static final String BASE_URL = "http://food2fork.com/api/search?";
+    public static final String QUERY_PARAMETER = "q";
+    public static final String KEY_QUERY_PARAMETER = "key";
+    public static final String SECOND_QUERY_PARAMETER = ",";
+}
