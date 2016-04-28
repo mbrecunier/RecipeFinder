f64ee5ee2002a3cfc6e68b08d379cebe61890a47
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-28 16:59:50 -0700

    call FoodService.findRecipes from RecipesActivity and override onFailure and onResponse functions

--- app/src/main/java/com/example/guest/recipefinder/RecipesActivity.java
+++ app/src/main/java/com/example/guest/recipefinder/RecipesActivity.java
@@ -4,16 +4,23 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.IOException;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class RecipesActivity extends AppCompatActivity {
+    public static final String TAG = RecipesActivity.class.getSimpleName();
     @Bind(R.id.searchTextView) TextView mSearchTextView;
     @Bind(R.id.recipeListView) ListView mRecipeListView;
     private String[] recipes = new String[] {"Parmesan Roasted Potatoes", "Crash Hot Potatoes", "Mac and Cheese with Roasted Chicken, Goat Cheese, and Rosemary", "Stovetop Avocado Mac and Cheese", "Jalapeno Popper Grilled Cheese Sandwich" };
@@ -33,14 +40,26 @@ public class RecipesActivity extends AppCompatActivity {
         String ingredient1 = recipesIntent.getStringExtra("ingredient1");
         String ingredient2 = recipesIntent.getStringExtra("ingredient2");
         mSearchTextView.setText(String.format(res.getString(R.string.recipe_results), ingredient1, ingredient2));
+        getRecipes(ingredient1, ingredient1);
+    }
 
-        ArrayAdapter recipesAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, recipes);
-        mRecipeListView.setAdapter(recipesAdapter);
+    private void getRecipes(String ingredient1, String ingredient2) {
+        FoodService.findRecipes(ingredient1, ingredient1, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
 
-        mRecipeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> adapterView, View v, int i, long l) {
-                Intent detailsIntent = new Intent(RecipesActivity.this, RecipeDetailsActivity.class);
-                startActivity(detailsIntent);
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, jsonData);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
