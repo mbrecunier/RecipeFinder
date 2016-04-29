97a3882b0ea4d46553ec9def777a4062536bd017
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-28 17:13:45 -0700

    create Recipe class with properties for display

---
+++ app/src/main/java/com/example/guest/recipefinder/Recipe.java
@@ -0,0 +1,34 @@
+package com.example.guest.recipefinder;
+
+/**
+ * Created by Guest on 4/28/16.
+ */
+public class Recipe {
+    private String mName;
+    private String mImageUrl;
+    private String mSourceUrl;
+    private String mRecipeId;
+
+    public Recipe(String name, String imageUrl, String sourceUrl, String recipeId) {
+        this.mName = name;
+        this.mImageUrl = imageUrl;
+        this.mSourceUrl = sourceUrl;
+        this.mRecipeId = recipeId;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    public String getSourceUrl() {
+        return mSourceUrl;
+    }
+
+    public String getRecipeId() {
+        return mRecipeId;
+    }
+}
