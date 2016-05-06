643234728fb9646753a1e05729a5d767d1b56947
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-05-06 11:46:03 -0700

    create logout button on top menu to redirect user to login page

--- app/src/main/java/com/example/guest/recipefinder/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/recipefinder/ui/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.guest.recipefinder.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.guest.recipefinder.Constants;
 import com.example.guest.recipefinder.R;
+import com.firebase.client.Firebase;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -17,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Bind(R.id.ingredient1EditText) EditText mIngredient1EditText;
     @Bind(R.id.ingredient2EditText) EditText mIngredient2EditText;
     @Bind(R.id.savedRecipesButton) Button mSavedRecipesButton;
+    private Firebase mFirebaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +30,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
+
         mFindRecipesButton.setOnClickListener(this);
         mSavedRecipesButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void logout() {
+        mFirebaseRef.unauth();
+        takeUserToLoginScreenOnUnAuth();
+    }
+
+    private void takeUserToLoginScreenOnUnAuth() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 
     @Override
