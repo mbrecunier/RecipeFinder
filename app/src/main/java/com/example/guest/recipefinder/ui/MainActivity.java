74ea962dab0bd3eb10ca1501c016b0c80a10bcce
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-05-06 12:07:36 -0700

    add welcome message to MainActivity with user's name

--- app/src/main/java/com/example/guest/recipefinder/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/recipefinder/ui/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.guest.recipefinder.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.guest.recipefinder.Constants;
 import com.example.guest.recipefinder.R;
+import com.example.guest.recipefinder.models.User;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -23,6 +31,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Bind(R.id.ingredient2EditText) EditText mIngredient2EditText;
     @Bind(R.id.savedRecipesButton) Button mSavedRecipesButton;
     private Firebase mFirebaseRef;
+    private ValueEventListener mUserRefListener;
+    private Firebase mUserRef;
+    private String mUId;
+    private SharedPreferences mSharedPreferences;
+    @Bind(R.id.introTextView)TextView mWelcomeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +47,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mFindRecipesButton.setOnClickListener(this);
         mSavedRecipesButton.setOnClickListener(this);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mUId = mSharedPreferences.getString(Constants.KEY_UID, null);
+        mUserRef = new Firebase(Constants.FIREBASE_URL_USERS).child(mUId);
+
+        mUserRefListener = mUserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                mWelcomeTextView.setText("Welcome, " + user.getName() + "!");
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
     }
 
     @Override
